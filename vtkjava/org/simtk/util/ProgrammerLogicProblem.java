30029b0cd7d79e763cbe0ded646076deac089c4d
cmbruns <cmbruns@c8d2a07d-96ef-3d62-bddb-3f200a2d7b94>
2005-06-22 23:25:40 +0000

    Created new classes for uncompressing streams   compressed with UN*X compress, and for   throwing an error due to programmer logic   problems.

---
+++ vtkjava/org/simtk/util/ProgrammerLogicProblem.java
@@ -0,0 +1,11 @@
+/*
+ * Created on Jun 17, 2005
+ *
+ */
+package org.simtk.util;
+
+public class ProgrammerLogicProblem extends RuntimeException {
+    public static final long serialVersionUID = 1L;
+    public ProgrammerLogicProblem() {super();}
+    public ProgrammerLogicProblem(String msg) {super(msg);}
+}
