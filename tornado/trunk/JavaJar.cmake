6e7ba0e0d8748ba33ffc5d33bd302d683d7c317c
cmbruns <cmbruns@c8d2a07d-96ef-3d62-bddb-3f200a2d7b94>
2007-01-29 16:28:57 +0000

    Create new trunk based on version 0.11 branch

---
+++ tornado/trunk/JavaJar.cmake
@@ -0,0 +1,99 @@
+# Requires:
+#
+# These are specific to this directory
+# 1) ${JAR_FILE} - local name of jar file to create, minus "s_" prefix when signed
+# 2) ${JAR_INPUTS} - names of root directories and files to pack into jars - subversion files will be automatically removed
+# 3) ${CLASS_JAR_INPUTS} - directories to send unmodified into the jar file
+#
+# These should be set in a top level CMakeLists.text
+# 3) ${DO_SIGN_JARS} - whether to sign, and if so:
+#   3.1) ${JARSIGNER_KEYSTORE} filename of keystore
+#   3.2) ${JARSIGNER_USER_NAME} 
+#   3.3) ${JARSIGNER_PASSWORD}
+#
+
+# Create a location for newly created jar files
+SET(JAR_DIR ${CMAKE_BINARY_DIR}/jars)
+FILE(MAKE_DIRECTORY ${JAR_DIR})
+
+SET(JAR_FILE_PATH "${JAR_DIR}/${JAR_FILE}")
+
+# Enumerate all files for jar archive in JAR_INPUT_FILES_INTERNAL variable
+# Assume they are all directories for now... TODO - support files too
+SET(JAR_INPUT_FILES "")
+FOREACH(JAR_INPUT ${JAR_INPUTS})
+    # Divide into parent directory and name
+	GET_FILENAME_COMPONENT(JAR_INPUT_DIR ${JAR_INPUT} PATH)
+	FILE(RELATIVE_PATH JAR_INPUT_NAME ${JAR_INPUT_DIR} ${JAR_INPUT})
+	
+	# MESSAGE("JAR_INPUT = ${JAR_INPUT}")
+	# MESSAGE("JAR_INPUT_DIR = ${JAR_INPUT_DIR}")
+	# MESSAGE("JAR_INPUT_NAME = ${JAR_INPUT_NAME}")
+
+	# Enumerate all files in the subdirectory
+	FILE(GLOB_RECURSE JAR_INPUT_FILES_INTERNAL RELATIVE ${JAR_INPUT_DIR} ${JAR_INPUT}/*)
+
+	# MESSAGE("JAR_INPUT_FILES = ${JAR_INPUT_FILES_INTERNAL}")
+
+	# Do not use contents of .svn directories from the resource areas
+	# Remove all stretches containing ".svn" without semicolons
+	STRING(REGEX REPLACE "[^;]*\\.svn[^;]*" "" JAR_INPUT_FILES_INTERNAL "${JAR_INPUT_FILES_INTERNAL}")
+	# Remove artifactual multiple semicolons
+	STRING(REGEX REPLACE ";+" ";" JAR_INPUT_FILES_INTERNAL "${JAR_INPUT_FILES_INTERNAL}")
+	# Remove initial semicolon, if any
+	STRING(REGEX REPLACE "^;" "" JAR_INPUT_FILES_INTERNAL "${JAR_INPUT_FILES_INTERNAL}")
+	# Remove final semicolon, if any
+	STRING(REGEX REPLACE ";\$" "" JAR_INPUT_FILES_INTERNAL "${JAR_INPUT_FILES_INTERNAL}")
+
+	# Include "-C dir" directive for each file
+	#   Put one after each semicolon
+	STRING(REGEX REPLACE ";" ";-C ${JAR_INPUT_DIR};" JAR_INPUT_FILES_INTERNAL "${JAR_INPUT_FILES_INTERNAL}")	
+	#   And one at the very beginning
+	IF(JAR_INPUT_FILES_INTERNAL)
+		SET(JAR_INPUT_FILES_INTERNAL "-C ${JAR_INPUT_DIR}" ${JAR_INPUT_FILES_INTERNAL})
+	ENDIF(JAR_INPUT_FILES_INTERNAL)
+	
+	# Write filtered files to monolithic file list
+	SET(JAR_INPUT_FILES ${JAR_INPUT_FILES} ${JAR_INPUT_FILES_INTERNAL})
+ENDFOREACH(JAR_INPUT ${JAR_INPUTS})
+
+# Jar up EVERYTHING in the class directories
+FOREACH(JAR_INPUT ${CLASS_JAR_INPUTS})
+		SET(JAR_INPUT_FILES ${JAR_INPUT_FILES} "-C ${JAR_INPUT}" ".")
+ENDFOREACH(JAR_INPUT ${CLASS_JAR_INPUTS})
+
+# Write the source list to a file to avoid command line overflow
+SET(JAR_CONTENT_FILE ${CMAKE_BINARY_DIR}/${JAR_FILE}.contentlist)
+STRING(REGEX REPLACE ";" "\n" JAR_INPUT_FILES_LINES "${JAR_INPUT_FILES}")
+FILE(WRITE ${JAR_CONTENT_FILE} ${JAR_INPUT_FILES_LINES})
+
+# Create the jar command
+ADD_CUSTOM_COMMAND(
+	OUTPUT ${JAR_FILE_PATH}
+	COMMAND ${JAVA_ARCHIVE} -cf ${JAR_FILE_PATH} @${JAR_CONTENT_FILE}
+	DEPENDS ${JAR_CONTENT_FILE}
+	)
+ADD_CUSTOM_TARGET(
+	${JAR_FILE}-jar ALL
+	DEPENDS ${JAR_CONTENT_FILE} ${JAR_FILE_PATH}
+	)
+
+IF(DO_SIGN_JARS)
+	INCLUDE(${CMAKE_SOURCE_DIR}/SignJar.cmake)
+	
+	# Install the signed jar file
+	INSTALL_FILES(${MY_INSTALL_PREFIX}/bin
+		FILES
+		"${SIGNED_JAR_PATH}"
+		)
+
+ELSE(DO_SIGN_JARS)
+
+	# Only install the regular unsignedjar file if there is no signed jar file
+	INSTALL_FILES(${MY_INSTALL_PREFIX}/bin
+		FILES
+		"${JAR_FILE_PATH}"
+		)
+
+ENDIF(DO_SIGN_JARS)
+	
---
+++ tornado/trunk/src/org/simtk/mol/toon/TubeAndStickTrace.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2005, Stanford University. All rights reserved. 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions
+ * are met: 
+ *  - Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ *  - Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ *  - Neither the name of the Stanford University nor the names of its 
+ *    contributors may be used to endorse or promote products derived 
+ *    from this software without specific prior written permission. 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+/*
+ * Created on Apr 28, 2005
+ *
+ */
+package org.simtk.mol.toon;
+
+import org.simtk.molecularstructure.*;
+
+/** 
+ * @author Christopher Bruns
+ * 
+ * Tubes connecting backbone, plus rods for nucleotides
+ */
+public class TubeAndStickTrace extends CompositeCartoon {
+    double backboneRadius = 1.50;
+    double rodRadius = 0.50;
+
+    int stickResolution = 5;
+
+    BackboneStick tubes;
+    NucleotideStickCartoon rods;
+    
+    public TubeAndStickTrace() {
+        this(1.50, 0.5);
+    }
+
+    public TubeAndStickTrace(double r1, double r2) {
+        backboneRadius = r1;
+        rodRadius = r2;
+
+        tubes = new BackboneStick(backboneRadius);
+        rods = new NucleotideStickCartoon(rodRadius);
+        
+        addSubToon(tubes);
+        addSubToon(rods);
+    }
+    
+    public void addMolecule(Molecule m) {
+        tubes.addMolecule(m);
+        rods.addMolecule(m);
+    }
+}
